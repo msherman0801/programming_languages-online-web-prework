e1fe556d0e3bd043b155033950a28ccba1958df7
Matthew Sherman <msherman0801@gmail.com>
2018-11-02 00:28:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
-def reformat_languages(languages)
-  # your code here
+def reformat_languages(hash)
+  newHash = {}
+  hash.each do |key, val|
+    val.each do |lang, attr|
+      newHash[lang] = {:type => attr.values.join(''), :style => []}
+      if hash[key][lang]
+       newHash[lang][:style].push(key)
+      end
+    end
+  end
+  newHash
 end
