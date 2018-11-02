267008d7391f2f85709ec9f7efea66fcc8214d38
Matthew Sherman <msherman0801@gmail.com>
2018-11-02 00:48:51 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,12 +2,12 @@ def reformat_languages(hash)
   newHash = {}
   hash.each do |key, val|
     val.each do |lang, attr|
-      newHash[lang] = {:type => attr.values.join(''), :style => []}
-      if hash[key][lang]
-        newHash[lang][:style].push(key)
+      if newHash[lang] == nil
+        newHash[lang] = {:type => attr.values.join(''), :style => [key]}
+      else
+        newHash[lang][:style] << key
       end
     end
-    newHash[:javascript][:style].push(:oo)
   end
   newHash
 end
